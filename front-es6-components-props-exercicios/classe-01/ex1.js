606bfea081e1d034fb039ab8ba3551503638b20b
Jeferson Vieira <jeferson.vm1981@gmail.com>
2024-01-24 19:30:10 -0300

    Exercício 01 concluído

---
+++ front-es6-components-props-exercicios/classe-01/ex1.js
@@ -0,0 +1,13 @@
+// function sum(...rest) {
+//   return a + b ;
+// }
+
+// console.log(sum(1, 3, 4, 5, 6, 7, 8, 9, 0, 5));
+
+const soma = (...args) => {
+  const somaNumeros = args.reduce((acc, curr) => acc + curr, 0);
+
+  return somaNumeros;
+};
+
+console.log(soma(1, 3, 4, 5, 6, 7, 8, 9, 0, 5));
