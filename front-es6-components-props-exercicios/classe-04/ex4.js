0a0752f4cb67d5b722dc0ba95bfe8f18f0ea16b2
Jeferson Vieira <jeferson.vm1981@gmail.com>
2024-01-25 00:47:06 -0300

    Exercício 04 concluído

--- front-es6-components-props-exercicios/classe-04/ex4.js
+++ front-es6-components-props-exercicios/classe-04/ex4.js
@@ -2,10 +2,10 @@ const classA = ["Daniel", "Guido", "Léo", "Vitor", "Ruliana", "Diego", "José"]
 const classB = ["Marcos", "Maria", "Juliana", "Cristina", "Paula"];
 const classC = ["Marcela", "Pedro", "João", "Jorge", "Carlos", "Henrique"];
 
-let allClasses = classA.concat(classB);
+allClasses = [...classA, ...classB];
 
-allClasses = allClasses.concat(classC[0], classC[1], classC[4], classC[5]);
+const [zero, one, , , four, five] = classC;
+
+allClasses = [...allClasses, zero, one, four, five];
 
 console.log(allClasses);
-
-
